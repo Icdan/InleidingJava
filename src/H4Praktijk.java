bdb92b8d89adecfde60f9c68831c6aa7686d5a0a
Icdan <li215@live.nl>
2017-09-05 14:39:28 +0200

    H4 Praktijk partial

---
+++ src/H4Praktijk.java
@@ -0,0 +1,48 @@
+import java.applet.*;
+import java.awt.*;
+
+public class H4Praktijk extends Applet {
+
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+
+        //Lijn
+        g.setColor(Color.black);
+        g.drawLine(5, 5, 210, 5);
+        g.drawString("Lijn", 30, 20);
+
+        //Rechthoek
+        g.drawRect(5, 40, 205, 150);
+        g.drawString("Rechthoek", 85, 205);
+
+        //Gevulde rechthoek met ovaal
+        g.setColor(Color.magenta);
+        g.fillRect(220,40,206,151);
+        g.setColor(Color.black);
+        g.drawOval(220, 40, 205, 150);
+        g.drawString("Gevulde rechthoek met ovaal", 240, 205);
+
+        //Taartpunt met ovaal eromheen
+        g.setColor(Color.magenta);
+        g.fillArc(475, 10, 250, 175, 0, 45);
+        g.setColor(Color.black);
+        g.drawArc(475, 10, 250, 175, 0, 360);
+        g.drawString("Taartpunt met ovaal eromheen", 515, 205);
+
+        //Afgeronde rechthoek
+        g.drawRoundRect(5, 225, 205, 150, 30, 30);
+        g.drawString("Afgeronde rechthoek", 55, 400);
+
+        //Gevulde ovaal
+        g.setColor(Color.magenta);
+        g.fillOval(220, 225, 205, 150);
+
+        //Cirkel
+        g.drawOval(475, 225,100, 100);
+
+    }
+}
