035451011c92fc39305593a1218160bf7ec7203c
Icdan <li215@live.nl>
2017-09-05 13:50:55 +0200

    4.7

---
+++ src/H4Punt7.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.applet.*;
+
+
+public class H4Punt7 extends Applet {
+
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.white);
+        g.setColor(Color.black);
+        g.drawRoundRect(10, 10, 40, 40, 10, 10);
+        g.drawOval(15, 15, 10, 10);
+        g.drawOval(35, 15, 10, 10);
+        g.drawOval(15, 35, 10, 10);
+        g.drawOval(35, 35, 10, 10);
+
+    }
+
+}
