86ef45786cc1a914cfcd0e9644ca8c93f01aed57
Icdan <li215@live.nl>
2017-09-05 13:33:12 +0200

    4.5

---
+++ src/H4Punt5.java
@@ -0,0 +1,17 @@
+import java.awt.*;
+import java.applet.*;
+
+
+public class H4Punt5 extends Applet {
+
+
+    public void init() {
+    }
+
+    public void paint(Graphics g) {
+        setBackground(Color.blue);
+        g.setColor(Color.yellow);
+        g.fillArc(100, 75, 150, 150, 90, 360);
+    }
+
+}
