07b57c853f3f1f26186eaa071a97bb7c25be2eb4
Icdan <li215@live.nl>
2017-09-08 10:39:56 +0200

    H6 Praktijk done

--- src/H6Praktijk.java
+++ src/H6Praktijk.java
@@ -2,19 +2,24 @@ import java.applet.*;
 import java.awt.*;
 
 public class H6Praktijk extends Applet {
-    double a, b, c, d, e;
-
+    double a, b, c, d, e, f, h, i, l;
+    
     public void init() {
         a = 5.9;
         b = 6.3;
         c = 6.9;
         d = 3;
-        e = a + b + c / e;
+        e = 10;
+        f = (a + b + c) / d;
+        h = f * e;
+        i = (int) h;
+        l = i / e;
     }
 
     public void paint(Graphics g) {
         g.setColor(Color.black);
-        g.drawString("Het gemiddelde is: " + e, 20, 20);
+        g.drawString("Het gemiddelde is: " + l, 20, 20);
+
     }
 
 }
