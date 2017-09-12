97b558fa9a0a4318a7c80a7436a55c77c7b9bd8f
Icdan <li215@live.nl>
2017-09-12 13:34:22 +0200

    Finished 8.1

--- src/H8Punt1.java
+++ src/H8Punt1.java
@@ -6,11 +6,13 @@ public class H8Punt1 extends Applet {
     Button ButtonA;
     Button ButtonB;
     String schermtekst;
+    String vervangtekst;
     TextField tekstvak;
     Label label;
 
+
     public void init () {
-        ButtonA = new Button( "Click on me");
+        ButtonA = new Button( "Ok");
             ButtonAListener BAl = new ButtonAListener();
             ButtonA.addActionListener(BAl);
             add(ButtonA);
@@ -20,23 +22,29 @@ public class H8Punt1 extends Applet {
             ButtonB.addActionListener(BBl);
             add(ButtonB);
 
-        schermtekst = "Does this work?";
+        schermtekst = "Your text will appear here after you press Ok";
 
-        tekstvak = new TextField("", 40);
+        tekstvak = new TextField("Type here", 40);
             add(tekstvak);
         label = new Label("");
+        tekstvak.addActionListener( new TekstvakListener() );
             add(label);
             add(tekstvak);
+
+        vervangtekst = "";
+
+
+
     }
 
     public void paint (Graphics g) {
-        g.drawString(schermtekst, 150, 75);
+        g.drawString(schermtekst, 50, 75);
 
     }
 
     class ButtonAListener implements ActionListener {
-        public void actionPerformed(ActionEvent e) {
-            tekstvak.setText("Your button works");
+        public void actionPerformed(ActionEvent BAl) {
+            vervangtekst = tekstvak.getText();
             repaint();
         }
     }
@@ -45,9 +53,17 @@ public class H8Punt1 extends Applet {
         public void actionPerformed(ActionEvent f) {
             tekstvak.setText("");
             repaint();
-            }
-
-
+        }
     }
+
+    class TekstvakListener implements ActionListener {
+        public void actionPerformed(ActionEvent i) {
+            vervangtekst = tekstvak.getText();
+            schermtekst = vervangtekst.replace("","");
+            repaint();
+
+        }
+    }
+
 
 }
