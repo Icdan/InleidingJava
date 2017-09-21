a15b2231d67f679e01484cf35d363a2a118199f1
Icdan <li215@live.nl>
2017-09-21 13:30:11 +0200

    H10Punt5

---
+++ src/H10Punt5.java
@@ -0,0 +1,68 @@
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class H10Punt5 extends Applet {
+
+    TextField field1, field2, field3;
+    double cijfer1, cijfer2, cijfer3, cijfer4;
+    String resultaat1, resultaat2, resultaat3, uitkomst;
+    Button ButtonOk;
+
+    public void init() {
+        uitkomst = "";
+
+        ButtonOk = new Button("Ok");
+        FieldInvoerListener FIV = new FieldInvoerListener();
+        ButtonOk.addActionListener(FIV);
+        add(ButtonOk);
+
+        field1 = new TextField("", 5);
+        field1.addActionListener(new FieldInvoerListener());
+        add(field1);
+
+        field2 = new TextField("", 5);
+        field2.addActionListener(new FieldInvoerListener());
+        add(field2);
+
+        field3 = new TextField("", 5);
+        field3.addActionListener(new FieldInvoerListener());
+        add(field3);
+
+    }
+
+    public void paint(Graphics g){
+        g.drawString("" + uitkomst, 60, 40);
+    }
+
+    public class FieldInvoerListener implements ActionListener {
+        public void actionPerformed (ActionEvent e){
+            resultaat1 = field1.getText();
+            cijfer1 = Double.parseDouble(resultaat1);
+
+            resultaat2 = field2.getText();
+            cijfer2 = Double.parseDouble(resultaat2);
+
+            resultaat3 = field3.getText();
+            cijfer3 = Double.parseDouble(resultaat3);
+
+            cijfer4 = (cijfer1 + cijfer2 + cijfer3) / 3;
+
+            if ( cijfer4 > 5.5) {
+
+                uitkomst = "Uw gemiddelde is: " + cijfer4 + ". Dit is een voldoende.";
+
+            }
+
+            else  {
+
+                uitkomst = "Uw gemiddelde is: " + cijfer4 + ". Dit is geen voldoende.";
+
+
+            }
+
+            repaint ();
+        }
+    }
+
+}
