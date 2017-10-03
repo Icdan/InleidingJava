1bc77f07688a90109ae3c7bfb89bb16c7d120c4b
Icdan <li215@live.nl>
2017-10-03 14:20:48 +0200

    H11Praktijk1 done

--- src/H11/H11Praktijk1.java
+++ src/H11/H11Praktijk1.java
@@ -1,59 +1,51 @@
 package H11;
 
-import java.applet.* ;
-import java.awt.* ;
-import java.awt.event.* ;
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.*;
 
 public class H11Praktijk1 extends Applet {
     Button okKnop;
     TextField textfield;
-    int getal, teller, x, y;
-    String invoerstring,  uitkomst;
+    int teller, getal, x, y;
     Label label;
 
-    public void init (){
+    public void init(){
+        setSize(500,500);
+
+        textfield = new TextField("", 20);
+        label = new Label("Type hier uw getal");
+        textfield.addActionListener(new OkListener());
+        add(label);
+        add(textfield);
+
         okKnop = new Button("Ok");
         OkListener Ok = new OkListener();
         okKnop.addActionListener(Ok);
         add(okKnop);
 
-        textfield = new TextField("", 20);
-        label = new Label("Type uw getal");
-        textfield.addActionListener(new OkListener());
-        add(label);
-        add(textfield);
-
-        invoerstring = "";
-        getal = 0;
-
     }
 
     public void paint(Graphics g){
-
         x = 50;
         y = 50;
+
+        if (getal > 0) {
+
+            for (teller = 1; teller < 21; teller++) {
+                y += 20;
+                g.drawString(teller + " x " + getal + " = " + teller * getal, x, y);
+
+            }
+        }
+
     }
-        if (getal>0){
-            for (teller = 0; teller<10;teller++){
-                y = y+20;
 
-                g.drawString(teller + "x" + getal + "=" + getal*teller , x ,y);
-
-
-
-    }
-
-    public class OkListener implements ActionListener{
+    public class OkListener implements ActionListener {
         public void actionPerformed(ActionEvent e){
-
-            getal = Integer.parseInt(textfield.getText()));
+            getal = Integer.parseInt(textfield.getText());
             textfield.setText("");
             repaint();
-
-
-             }  }
-
-
         }
     }
-
+}
